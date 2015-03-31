32b88bc791dd4e13d07a8b4b172298cf6bc5d882
Ivan Shumkov <ivan@shumkov.ru>
2015-03-31 16:26:41 +0300

    add elastic module to skinny

--- bone.js
+++ bone.js
@@ -5,4 +5,5 @@ var elasticsearch = require('elasticsearch');
 module.exports = function attachElastic(skinny, options) {
     // See options at http://www.elastic.co/guide/en/elasticsearch/client/javascript-api/current/configuration.html
     skinny.elastic = new elasticsearch.Client(options);
+    skinny.elasticModule = elasticsearch;
 };
